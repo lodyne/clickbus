07b31912ce988805d41122b439a63e54fd8a3abf
lodyne <lodgmtui@gmail.com>
2022-04-21 16:26:16 +0300

    migrate(model):make model with different models
    ~ migrate model to the database
    ~ create superuser for admin functionality
    ~ install PILLOW library for Image Upload
    ~ add packages to requirements.txt file

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
+class Place():
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="media",height_field=None, width_field=None, max_length = None)
+    created_at = models.DateTimeField(auto_now=False,auto_now_add=False)
+    updated_at = models.DateTimeField(auto_now=False,auto_now_add=False)
+
+    def __str__(self):
+        return self.name
+    
