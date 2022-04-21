f862736f9460759ca22c42bb5ec4643996d64e96
lodyne <lodgmtui@gmail.com>
2022-04-21 21:45:06 +0300

    modify(model):modify model fields
    ~ register  model Place to admin panel
    ~ migrate model to the database

--- core/models.py
+++ core/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from PIL import Image
 
 # Create your models here.
-class Place():
+class Place(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     image = models.ImageField(upload_to="media",height_field=None, width_field=None, max_length = None)
-    created_at = models.DateTimeField(auto_now=False,auto_now_add=False)
-    updated_at = models.DateTimeField(auto_now=False,auto_now_add=False)
+    created_at = models.DateField(auto_now=False,auto_now_add=False)
+    updated_at = models.DateField(auto_now=False,auto_now_add=False)
 
     def __str__(self):
         return self.name
