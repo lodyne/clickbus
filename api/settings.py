66276626d5adc222f8c9f123e19a3335d9753ecb
lodyne <lodgmtui@gmail.com>
2022-04-22 22:47:54 +0300

    modify(settings):modify settings.py file

--- api/settings.py
+++ api/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 
 import os
+import dj_database_url
 from pathlib import Path
 from decouple import config
+
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,6 +108,11 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
@@ -165,6 +175,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
