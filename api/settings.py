7a2f2b33d288eb819d259720e386ce1ee8ac8c22
lodyne <lodgmtui@gmail.com>
2022-04-23 07:56:40 +0300

    changes(whitenoise):change whitenoise file storage

--- api/settings.py
+++ api/settings.py
@@ -167,7 +167,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 
