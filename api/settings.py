c5e669f46407173f7bf4f3c7a12d1256290f01e6
lodyne <lodgmtui@gmail.com>
2022-04-23 11:24:44 +0300

    fix(deploy):fix bugs during deploy

--- api/settings.py
+++ api/settings.py
@@ -34,11 +34,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast = bool)
 
-ALLOWED_HOSTS = [
-    'https://clickbus-api.herokuapp.com/',
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['clickbus-api.herokuapp.com']
 
 
 # Application definition
